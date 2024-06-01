79134afb5d99afdaddbc3c03249f2c794733f179
anoushka <vyasanoushka@gmail.com>
2024-06-01 17:25:00 +0530

    login and signup

---
+++ src/components/Hello.jsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react'
+
+const Hello = () => {
+  return (
+    <div>Hello me nuska
+      eeska muska now what? <br />
+      nothing
+    </div>
+  )
+}
+
+export default Hello
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import {Routes, Route} from 'react-router-dom'
 import Dashboard from "../pages/Dashboard";
-
+import Login from "../pages/Login";
+import SignUp from "../pages/SignUp";
 const Router = () => {
     return(
         <Routes>
             <Route path="/" element={<Dashboard/>}/>
+            <Route path="/signup" element={<SignUp/>}/>
+            <Route path="/login" element={<Login/>}/>
         </Routes>
     )
 }
